7a9876ccf70f5336dba99192327590b63d0f19f3
liddqd <klund@tehpro.dk>
2022-01-27 01:28:12 +0100

    updated encoder code

--- keyboards/gmmk/pro/iso/keymaps/liddqd/keymap.c
+++ keyboards/gmmk/pro/iso/keymaps/liddqd/keymap.c
@@ -44,7 +44,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         KC_ESC,  KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12,  KC_PSCR,          KC_MUTE,
         KC_GRV,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_MINS, KC_EQL,  KC_BSPC,          KC_HOME,
         KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_LBRC, KC_RBRC,                   KC_PGUP,
-        KC_CAPS, KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT, KC_NUHS, KC_ENT,           KC_PGDN,
+        MO(_FN1), KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT, KC_NUHS, KC_ENT,           KC_PGDN,
         KC_LSFT, KC_NUBS, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH,          KC_RSFT, KC_UP,   KC_END,
         KC_LCTL, KC_LGUI, KC_LALT,                            KC_SPC,                             KC_RALT, MO(_FN1),   KC_RCTL, KC_LEFT, KC_DOWN, KC_RGHT
     ),
@@ -202,20 +202,37 @@ bool encoder_update_user(uint8_t index, bool clockwise) {
 bool encoder_update_user(uint8_t index, bool clockwise) {
     uint8_t mods_state = get_mods();
     if (mods_state & MOD_BIT(KC_LSFT) ) { // If you are holding L shift,
-        //
+       
        if (clockwise) {
-           tap_code(KC_Q);
+           tap_code(KC_J);
        } else {
-           tap_code(KC_A);
+           tap_code(KC_K);
        }
-    } else if (mods_state & MOD_BIT(KC_RSFT) ) { // If you are holding R shift,
-        //
+    /*} else if (mods_state & MOD_BIT(KC_RSFT) ) { // If you are holding R shift,
+       if (clockwise) {
+           tap_code(KC_C);
+       } else {
+           tap_code(KC_D);
+       }
     } else if (mods_state & MOD_BIT(KC_LCTL)) {  // if holding Left Ctrl,
-        //
+       if (clockwise) {
+           tap_code(KC_E);
+       } else {
+           tap_code(KC_F);
+       }
     } else if (mods_state & MOD_BIT(KC_RCTL)) {  // if holding Right Ctrl,
-        //
+       if (clockwise) {
+           tap_code(KC_G);
+       } else {
+           tap_code(KC_H);
+       }
     } else if (mods_state & MOD_BIT(KC_LALT)) {  // if holding Left Alt,
-        //
+       if (clockwise) {
+           tap_code(KC_I);
+       } else {
+           tap_code(KC_J);
+       }
+       */ 
     } else  {
         switch(get_highest_layer(layer_state)) {
             case _FN1:
