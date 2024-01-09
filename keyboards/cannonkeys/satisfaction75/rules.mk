09c8e0adceb6c9fc1fb39687e29a20a9e2d1558d
liddqd <klund@tehpro.dk>
2024-01-09 13:16:08 +0100

    remove invalid options in rules.mk

--- keyboards/cannonkeys/satisfaction75/rules.mk
+++ keyboards/cannonkeys/satisfaction75/rules.mk
@@ -16,8 +16,6 @@ COMMAND_ENABLE = yes    # Commands for debug and configuration
 NKRO_ENABLE = yes           # Enable N-Key Rollover
 ENCODER_ENABLE = yes
 OLED_ENABLE = yes
-OLED_DRIVER_ENABLE = yes
-OLED_DRIVER = SSD1306
 #VIA_ENABLE = yes
 WPM_ENABLE = yes
 
